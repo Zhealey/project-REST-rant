4ab77e7bcbb196ad5e2cb14da86d53a5033c45b4
ZHealey <zackhealey@yahoo.com>
2022-05-04 19:29:10 -0400

    adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send('GET /places')
+router.get('/new', (req, res) => {
+  res.render('places/new')
 })
+
   let places = [
     {
       name: "H-Thai-ML",
